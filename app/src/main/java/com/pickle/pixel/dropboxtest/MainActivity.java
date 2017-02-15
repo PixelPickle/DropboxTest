2165bb4b4e10f7f27b0c08faf5f669b9192773ed
PixelPickle <bates.kaylen@gmail.com>
2017-02-15 00:43:47 -0600

    Created UI
    Made UI, next is create downloader

--- app/src/main/java/com/pickle/pixel/dropboxtest/MainActivity.java
+++ app/src/main/java/com/pickle/pixel/dropboxtest/MainActivity.java
@@ -7,16 +7,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 import com.dropbox.chooser.android.DbxChooser;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final int DBX_CHOOSER_REQUEST = 0;  // You can change this if needed
+    static final int CHOOSER_SPRITE_SHEET = 0;
+    static final int CHOOSER_TILESET = 1;
 
     private static final String APP_KEY = "ziz3befif8nlh3p";
 
     private Button mChooserButton;
     private DbxChooser mChooser;
+
+    private android.net.Uri containerTileset;
+    private android.net.Uri containerSpriteSheet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +31,21 @@ public class MainActivity extends AppCompatActivity {
 
         mChooser = new DbxChooser(APP_KEY);
 
-        mChooserButton = (Button) findViewById(R.id.chooser_button);
+        mChooserButton = (Button) findViewById(R.id.btnImportSprite);
+        mChooserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mChooser.forResultType(DbxChooser.ResultType.DIRECT_LINK)
+                        .launch(MainActivity.this, CHOOSER_SPRITE_SHEET);
+            }
+        });
+
+        mChooserButton = (Button) findViewById(R.id.btnImportTile);
         mChooserButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mChooser.forResultType(DbxChooser.ResultType.PREVIEW_LINK)
-                        .launch(MainActivity.this, DBX_CHOOSER_REQUEST);
+                        .launch(MainActivity.this, CHOOSER_TILESET);
             }
         });
 
@@ -38,20 +53,31 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == DBX_CHOOSER_REQUEST) {
-            if (resultCode == Activity.RESULT_OK) {
-                DbxChooser.Result result = new DbxChooser.Result(data);
-                Log.d("main", "Link to selected file: " + result.getLink());
+        if (requestCode == CHOOSER_SPRITE_SHEET && resultCode == Activity.RESULT_OK) {
+            DbxChooser.Result result = new DbxChooser.Result(data);
+            Log.d("main", "Link to selected file: " + result.getLink());
 
-                // Handle the result
-                Log.d("main", "Name of File: " + result.getName());
-            } else if (resultCode == Activity.RESULT_CANCELED) {
+            // Handle the result
+            containerSpriteSheet = result.getLink();
+            Log.d("main", "Name of File: " + result.getName());
+            EditText text = (EditText) findViewById(R.id.editSpriteSheet);
+            text.setText(result.getName());
+        } else if (requestCode == CHOOSER_TILESET && resultCode == Activity.RESULT_OK) {
+            DbxChooser.Result result = new DbxChooser.Result(data);
+            Log.d("main", "Link to selected file: " + result.getLink());
+
+            // Handle the result
+            containerTileset = result.getLink();
+            Log.d("main", "Name of File: " + result.getName());
+            EditText text = (EditText) findViewById(R.id.editTileSheet);
+            text.setText(result.getName());
+        } else if ( (requestCode == CHOOSER_TILESET || requestCode == CHOOSER_SPRITE_SHEET ) &&
+                resultCode == Activity.RESULT_CANCELED) {
                 // Cancelled by the user.
                 Log.d("main", "Cancelled by User");
-            } else {
+        } else if (requestCode == CHOOSER_TILESET || requestCode == CHOOSER_SPRITE_SHEET ){
                 // Failed
                 Log.d("main", "Failed for some reason");
-            }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
